39cc6e3200ef215ed5dd50e9cfc283fa079181bd
Dave <doble.aguila@hotmail.com>
2024-02-15 20:06:57 -0300

    Avances en controllers/usersRoutes

---
+++ server/src/controllers/createUser.js
@@ -0,0 +1,44 @@
+const { Users } = require("../db");
+const bcrypt = require('bcrypt');
+
+const createUser = async (req , res) => {
+    try {
+        const { username, email, password, avatar_img, master , admin} = req.body;
+
+        if(!username || !email || !password) return res.status(400).send("Faltan datos");
+        
+        if (username && email) {
+            const saltRounds = 10; //Número de rondas de cifrado
+            const hashedPassword = await bcrypt.hash(password , saltRounds);
+
+            const [register, created] = await Users.findOrCreate({
+                where: {username,email},
+                defaults: {username, email, password:hashedPassword, avatar_img, master, admin}
+            });
+
+            if (created) {
+                const response = {       
+                    id: register.id,
+                    username: register.username,
+                    email: register.email,
+                    password: register.password,
+                    avatar_img: register.avatar_img,
+                    master: register.master,
+                    admin: register.admin
+                };
+
+                return res.json(response);
+
+            } else {
+                return res.status(400).send("The user already exists, try another one");
+            }
+        };
+
+    } catch (error) {
+        
+        return res.status(500).json({ error: error.message });
+
+    }
+};
+
+module.exports = createUser;
