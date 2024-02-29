47fa8785170d2f7a0b1fbd6d30d3e067f669764f
Dave <doble.aguila@hotmail.com>
2024-02-29 01:32:22 -0300

    client/src/components/Landing(Por finalizar).

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,20 +1,34 @@
 import style from './Landing.module.css';
 import Button from 'react-bootstrap/Button';
+import imagePc from '../../assets/scrum.png'
+import curve from '../../assets/desktop-curve.svg';
+import imageBoard from '../../assets/decorative1.png'
 
 const Landing = () => {
 
     return (
         <div className={style.container}>
-        <h1>Bienvenido a TaskUnity</h1>
-        <h4>Gestiona tus proyectos personales y grupales de forma ordenada basándote en la metodología Scrum.</h4>
-        <h5>TaskUnity está inspirada en Trello y Jira, ofreciendo el mismo ordenamiento de Tablas, Listas y Tarjetas.</h5>
+
+        <div className={style.container_h1_img_h5}>
+        <h1>Amplifica tu productividad con <span>TaskUnity</span></h1>
+        <img className={style.image_pc} src={imagePc} alt="scrum" />
+        <h5>Gestiona tus proyectos personales y grupales de forma ordenada basándote en la metodología Scrum.</h5>
+        <img className={style.curve_black} src={curve} alt="curve" />
+        </div>
+
+        <div className={style.container_h5}>
+        <div className={style.container_h5_row}>
+        <h5>La aplicación está inspirada en Trello y Jira, ofreciendo el mismo ordenamiento de Tablas, Listas y Tarjetas.</h5>
+        <img className={style.image_board} src={imageBoard} alt="board" />
+        </div>
+        <div>
+        <img className={style.curve_white} src={curve} alt="curve"/>
+        </div>
+        </div>
+
         <div className={style.container_h5_btn}>
         <h5>¡Anímate y organiza tus proyectos!</h5>
-        <Button style={{
-            width:'200px',
-            height:'60px'
-            }}
-            variant="primary" size="lg">
+        <Button variant="primary" size="lg">
           Regístrate
         </Button>
         </div>
