261b7ddaa163088234afdd13679a4002cd40631a
Dave <doble.aguila@hotmail.com>
2024-03-01 20:02:10 -0300

    C:/Program Files/Git/components/About(Iniciando)&/NavContainer(Lógica para cerrar NavMenu al utilizar useNavigate)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes , Route } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
 import Nav from './components/NavContainer/NavContainer';
 import Footer from './components/Footer/Footer';
+import About from './components/About/About';
 
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
       <Nav/>
     <Routes>
       <Route path="/" element={<Landing/>}/>
+      <Route path="/about" element={<About/>}/>
     </Routes>
     <Footer/>
     </div>
---
+++ client/src/components/About/About.jsx
@@ -0,0 +1,16 @@
+import style from './About.module.css';
+
+const About = () => {
+
+    return (
+        <div className={style.container}>
+            <h1>About</h1>
+            <p>Detalle de about</p>
+            
+            <h2>¡Contact Me!</h2>
+            <p>Detalle de contacto</p>
+        </div>
+    )
+};
+
+export default About;
