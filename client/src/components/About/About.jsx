be0a44b3f554d02f7872a629997600afd3229269
Dave <doble.aguila@hotmail.com>
2024-03-01 21:50:09 -0300

    C:/Program Files/Git/components/About(Avanzando estilos)

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -4,11 +4,14 @@ const About = () => {
 
     return (
         <div className={style.container}>
+            <div className={style.first_container}>
             <h1>About</h1>
-            <p>Detalle de about</p>
-            
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci fugit sunt tempore veritatis, id iure enim maxime maiores quod magni cum modi sint ratione! Molestias minus ipsum sit rerum.</p>
+            </div>
+            <div className={style.second_container}>
             <h2>¡Contact Me!</h2>
-            <p>Detalle de contacto</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio impedit laborum quo beatae expedita? Ea error incidunt ex deleniti quod recusandae, ullam corporis! Voluptas vitae officia veniam. Veritatis, quibusdam distinctio.</p>
+            </div>
         </div>
     )
 };
