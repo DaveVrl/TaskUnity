e83bbd4d69fdb3dceb96a97890030342603ac690
Dave <doble.aguila@hotmail.com>
2024-03-07 00:44:58 -0300

    Avances estilos Booklet

--- client/src/components/Landing/Booklet/BookletOne.jsx
+++ client/src/components/Landing/Booklet/BookletOne.jsx
@@ -1,12 +1,12 @@
 import style from './Booklet.module.css';
-import image from '../../../assets/tablero-image.png';
+import image from '../../../assets/image-ideas.png';
 
 const BookletOne = () => {
 
     return (
         <div className={style.container}>
             <img src={image} alt="board-image" />
-            <p>El Tablero permite mantener tu proyecto organizado, podrás observar todo lo que se esté desarrollando en tan solo un vistazo.</p>
+            <h5>El Tablero permite mantener tu proyecto organizado, podrás observar todo lo que se esté desarrollando en tan solo un vistazo.</h5>
         </div>
     )
 };
