8579b7c530269e1ff012d3b8c7e45d2fc175c7eb
Dave <doble.aguila@hotmail.com>
2024-03-11 15:59:07 -0300

    C:/Program Files/Git/server/models/Workspaces(Modelo creado)

---
+++ server/src/models/Workspaces.js
@@ -0,0 +1,47 @@
+const { DataTypes } = require('sequelize');
+
+const Workspaces = (sequelize) => {
+
+    sequelize.define('workspaces' , {
+
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+
+        name: {
+            type: DataTypes.STRING
+        },
+
+        created_by: {
+            type: DataTypes.INTEGER
+        },
+
+        description: {
+            type: DataTypes.STRING
+        },
+
+        members_id: {
+            type: DataTypes.ARRAY(DataTypes.INTEGER),
+            defaultValue: []
+        },
+
+        admins_id: {
+            type: DataTypes.ARRAY(DataTypes.INTEGER),
+            defaultValue: []
+        },
+        
+        public: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+
+        logo: {
+            type: DataTypes.STRING
+        }
+    });
+};
+
+module.exports = Workspaces;
