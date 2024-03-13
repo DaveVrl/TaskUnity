450010e56d28d2cd9b4c3d6212bb52db90a31944
Dave <doble.aguila@hotmail.com>
2024-03-13 16:48:29 -0300

    C:/Program Files/Git/server/controllers/comments/(update & delete finalizados).

--- server/src/routes/commentsRoutes.js
+++ server/src/routes/commentsRoutes.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
 
 const createComment = require('../controllers/comments/createComment');
+const updateComment = require('../controllers/comments/updateComment');
+const deleteComment = require('../controllers/comments/deleteComment');
 
 const commentsRoutes = Router();
 
 commentsRoutes.post('/card/:cardId/comment',createComment);
+commentsRoutes.put('/comment/:id',updateComment);
+commentsRoutes.delete('/comment/:id',deleteComment);
 
 
 
